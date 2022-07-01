6316e8b910a727a1421a3054ebe8e4b619f4cb4d
Adrian Bjørdal <Findingnelly@proton.me>
2022-07-01 19:15:07 +0200

    Load Next Level

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -10,20 +10,25 @@ public class CollisionHandler : MonoBehaviour
     const string Finish = "Finish";
     const string Friendly = "Friendly";
 
+    private void Update()
+    {
+        
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
         {
-            case Finish:
-                Debug.Log("fin");
-                break;
+            
             case Friendly:
                 Debug.Log("friend");
                 break;
+            case Finish:
+                LoadNextLevel();
+                break;
             default:
                 ReloadLevel();
-
                 break;
 
         }
@@ -32,7 +37,16 @@ public class CollisionHandler : MonoBehaviour
             int activeScene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(activeScene);
         }
-        
+        void LoadNextLevel()
+        {
+            int activeScene = SceneManager.GetActiveScene().buildIndex;
+            int nextScene = activeScene + 1;
+            if (nextScene == SceneManager.sceneCountInBuildSettings)
+            {
+                nextScene = 0;
+            }
+            SceneManager.LoadScene(nextScene);   
+        }
      
 
 
